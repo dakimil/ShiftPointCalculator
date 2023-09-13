ae70e434141e175b7b30b0f072463c936fc44324
Дамјан Миленковић <damjanmilenkovic05@outlook.com>
2023-09-13 22:20:28 +0200

    Version2 stub

---
+++ Version2/ShiftPointCalculator/ShiftPointCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ShiftPointCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+
+            //ucitavanje fajla
+            //string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //string fullName = System.IO.Path.Combine(desktopPath, "Primer ulaznog fajla.txt");
+            String fullName = @"VehicleData\Primer ulaznog fajla.txt";
+            List<string> linije = new List<string>();
+        }
+    }
+}
