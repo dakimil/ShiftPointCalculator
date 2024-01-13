f56e51bc2bd5907706ca692146b2c9b5d39db5e6
Дамјан Миленковић <damjanmilenkovic05@outlook.com>
2024-01-13 17:52:42 +0100

    Zavrsio fajl

--- Version2/ShiftPointCalculator/ShiftPointCalculator/StepenPrenosaMenjaca.sql
+++ Version2/ShiftPointCalculator/ShiftPointCalculator/StepenPrenosaMenjaca.sql
@@ -7,4 +7,16 @@
  CONSTRAINT PK_StepenPrenosaMenjaca PRIMARY KEY CLUSTERED 
 (
 	Id ASC
-);
+)
+)
+GO
+CREATE UNIQUE INDEX IX_StepenPrenosaMenjaca_UNIQUE ON dbo.StepenPrenosaMenjaca (VoziloId, RedniBroj)
+GO
+ALTER TABLE dbo.StepenPrenosaMenjaca ADD CONSTRAINT FK_StepenPrenosaMenjaca_Vozilo FOREIGN KEY(VoziloId)
+REFERENCES dbo.Vozilo (Id)
+GO
+ALTER TABLE dbo.StepenPrenosaMenjaca ADD CONSTRAINT CK_StepenPrenosaMenjaca_RedniBroj CHECK
+(
+	RedniBroj IN (1, 2, 3, 4, 5, 6) 
+)
+GO
