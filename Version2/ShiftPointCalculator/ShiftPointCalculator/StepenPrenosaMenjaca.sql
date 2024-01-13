cc40413b3eeb1ca6c2de50896b8e9a8dbe3311c7
Дамјан Миленковић <damjanmilenkovic05@outlook.com>
2024-01-13 17:18:09 +0100

    Dodat StepenPrenosaMenjaca.Sql

---
+++ Version2/ShiftPointCalculator/ShiftPointCalculator/StepenPrenosaMenjaca.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE dbo.StepenPrenosaMenjaca
+(
+	Id int IDENTITY(1,1) NOT NULL,
+	VoziloId int NOT NULL,
+	RedniBroj int NOT NULL,
+	PrenosniOdnos decimal(4, 3) NOT NULL,
+ CONSTRAINT PK_StepenPrenosaMenjaca PRIMARY KEY CLUSTERED 
+(
+	Id ASC
+);
