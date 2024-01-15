1815487500bd32199f0eec436365f8c08c1da12a
Дамјан Миленковић <damjanmilenkovic05@outlook.com>
2024-01-15 21:46:39 +0100

    Added DataAcces folder and ConnectionStrings file

---
+++ Version2/ShiftPointCalculator/ShiftPointCalculator/DataAcces/ConnectionStrings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftPointCalculator.Repositories
+{
+    internal static class ConnectionStrings
+    {
+        public static string ConnectionString { get; set; } = @"Server=tcp:mihailosqlserver.database.windows.net,1433;Initial Catalog=ShiftPointCalculator;Persist Security Info=False;User ID=Mihailo;Password=Mika2211;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+    }
+}
