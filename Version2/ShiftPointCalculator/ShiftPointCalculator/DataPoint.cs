d995a1633549468c5c54cfc63cc7ca8ba7c39421
Дамјан Миленковић <damjanmilenkovic05@outlook.com>
2024-01-20 12:17:26 +0100

    Renamed prenosniOdnosUDiferencijalu to glavniPrenos in DataPoint.cs

--- Version2/ShiftPointCalculator/ShiftPointCalculator/DataPoint.cs
+++ Version2/ShiftPointCalculator/ShiftPointCalculator/DataPoint.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ShiftPointCalculator
 {
-    internal class DataPoint
+    public class DataPoint
     {
         public decimal MomentNaTocku { get; set; }
         public decimal MomentMotoraPriObrtajima { get; set; }
@@ -14,10 +14,14 @@ namespace ShiftPointCalculator
         public decimal PrenosniOdnosMenjaca { get; set; }
         public int RedniBrojStepenaPrenosaMenjaca { get; set; }
         public decimal BrzinaVozila { get; set; }
-        public void IzracunajBrojObrtajaMotora(decimal brz, decimal prenosniOdnosDiferencijala, decimal PoluprecnikTocka, List<StepenPrenosaMenjaca> StepeniPrenosaMenjaca)
+        public void IzracunajBrojObrtajaMotora(
+            decimal brzina, 
+            decimal glavniPrenos, 
+            decimal poluprecnikTocka, 
+            List<StepenPrenosaMenjaca> stepeniPrenosaMenjaca)
         {
-            this.BrojObrtajaMotora = brz * (1000.00m / 3600.00m) * prenosniOdnosDiferencijala / (PoluprecnikTocka / 1000.00m)
-                * StepeniPrenosaMenjaca[RedniBrojStepenaPrenosaMenjaca - 1].PrenosniOdnos * 60.00m / (2.00m * 3.1415m);
+            this.BrojObrtajaMotora = brzina * (1000.00m / 3600.00m) * glavniPrenos / (poluprecnikTocka / 1000.00m)
+                * stepeniPrenosaMenjaca[RedniBrojStepenaPrenosaMenjaca - 1].PrenosniOdnos * 60.00m / (2.00m * 3.1415m);
         }
         public void IzracunajMomentMotoraPriObrtajima(List<MomentMotora> momentiMotora)
         {
@@ -35,12 +39,13 @@ namespace ShiftPointCalculator
             if (gornjiMoment != null
             && donjiMoment != null)
             {
-                this.MomentMotoraPriObrtajima = donjiMoment.Value + ((this.BrojObrtajaMotora - (donjiObrtajiZaokrNa100 * 100m)) * ((gornjiMoment.Value - donjiMoment.Value) / 100m));
+                this.MomentMotoraPriObrtajima = donjiMoment.Value + ((this.BrojObrtajaMotora - (donjiObrtajiZaokrNa100 * 100m)) * 
+                    ((gornjiMoment.Value - donjiMoment.Value) / 100m));
             }
         }
-        public void IzaracunajMomentNaTockovima(decimal prenosniOdnosDiferencijala)
+        public void IzaracunajMomentNaTockovima(decimal glavniPrenos)
         {
-            this.MomentNaTocku = this.MomentMotoraPriObrtajima * this.PrenosniOdnosMenjaca * prenosniOdnosDiferencijala;
+            this.MomentNaTocku = this.MomentMotoraPriObrtajima * this.PrenosniOdnosMenjaca * glavniPrenos;
         }
 
     }
