ccf9d02f3215407c1cdcd9946fb7c295b9b6924b
Дамјан Миленковић <damjanmilenkovic05@outlook.com>
2024-06-09 19:45:54 +0200

    line endings

--- Version2/ShiftPointCalculator/ShiftPointCalculator/UI/UcitajIzvrsitelj.cs
+++ Version2/ShiftPointCalculator/ShiftPointCalculator/UI/UcitajIzvrsitelj.cs
@@ -1,5 +1,6 @@
 ﻿using ShiftPointCalculator.BusinessLogic;
 using ShiftPointCalculator.DataAcces;
+using System.Reflection;
 
 namespace ShiftPointCalculator.UI
 {
@@ -12,17 +13,29 @@ namespace ShiftPointCalculator.UI
             string filePath;
             Console.WriteLine("Unesite naziv i format fajla i ubacite ga u folder 'bin'");//prilagodi
             fileName = Console.ReadLine();
-            if(string.IsNullOrWhiteSpace(fileName) ) {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
                 Console.WriteLine("Uneli ste prazno ime fajla");
                 return false;
             }
+
+            /*
             filePath = $"C:\\Users\\Daki\\source\\repos\\ShiftPointCalculator\\Version2\\ShiftPointCalculator" +
                 $"\\ShiftPointCalculator\\bin\\Debug\\" +
                 $"net6.0\\VehicleData\\" + fileName;
+            */
+
+            Assembly? asm = Assembly.GetEntryAssembly();
+            
+            string? asmPath = asm!.Location;
+
+            string asmFolder = Path.GetDirectoryName(asmPath!);
+
+            filePath = Path.Combine(asmFolder!, "VehicleData", fileName);
 
             bool fileExists = File.Exists(filePath);
 
-            if(!fileExists)
+            if (!fileExists)
             {
                 Console.WriteLine($"File {filePath} nije pronadjen");
 
