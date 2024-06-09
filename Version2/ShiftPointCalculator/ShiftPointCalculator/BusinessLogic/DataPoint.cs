83d4c7245b7f9adbce62ca80a1281fbc541c1c4c
Дамјан Миленковић <damjanmilenkovic05@outlook.com>
2024-06-09 19:08:00 +0200

    premestene klase u BusinessLogic

--- Version2/ShiftPointCalculator/ShiftPointCalculator/BusinessLogic/DataPoint.cs
+++ Version2/ShiftPointCalculator/ShiftPointCalculator/BusinessLogic/DataPoint.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ShiftPointCalculator
+namespace ShiftPointCalculator.BusinessLogic
 {
     public class DataPoint
     {
@@ -15,37 +15,37 @@ namespace ShiftPointCalculator
         public int RedniBrojStepenaPrenosaMenjaca { get; set; }
         public decimal BrzinaVozila { get; set; }
         public void IzracunajBrojObrtajaMotora(
-            decimal brzina, 
-            decimal glavniPrenos, 
-            decimal poluprecnikTocka, 
+            decimal brzina,
+            decimal glavniPrenos,
+            decimal poluprecnikTocka,
             List<StepenPrenosaMenjaca> stepeniPrenosaMenjaca)
         {
-            this.BrojObrtajaMotora = brzina * (1000.00m / 3600.00m) * glavniPrenos / (poluprecnikTocka / 1000.00m)
+            BrojObrtajaMotora = brzina * (1000.00m / 3600.00m) * glavniPrenos / (poluprecnikTocka / 1000.00m)
                 * stepeniPrenosaMenjaca[RedniBrojStepenaPrenosaMenjaca - 1].PrenosniOdnos * 60.00m / (2.00m * 3.1415m);
         }
         public void IzracunajMomentMotoraPriObrtajima(List<MomentMotora> momentiMotora)
         {
-            decimal donjiObrtajiZaokrNa100 = Math.Floor(this.BrojObrtajaMotora / 100m);
-            Nullable<Decimal> donjiMoment = null, gornjiMoment = null;
-            for(int i = 0; i<momentiMotora.Count; i++)
+            decimal donjiObrtajiZaokrNa100 = Math.Floor(BrojObrtajaMotora / 100m);
+            decimal? donjiMoment = null, gornjiMoment = null;
+            for (int i = 0; i < momentiMotora.Count; i++)
             {
                 if (momentiMotora[i].BrojObrtaja == donjiObrtajiZaokrNa100 * 100m)
                 {
                     donjiMoment = momentiMotora[i].MomentPriObrtajima;
-                    gornjiMoment= momentiMotora[i+1].MomentPriObrtajima;
+                    gornjiMoment = momentiMotora[i + 1].MomentPriObrtajima;
                     break;
                 }
             }
             if (gornjiMoment != null
             && donjiMoment != null)
             {
-                this.MomentMotoraPriObrtajima = donjiMoment.Value + ((this.BrojObrtajaMotora - (donjiObrtajiZaokrNa100 * 100m)) * 
-                    ((gornjiMoment.Value - donjiMoment.Value) / 100m));
+                MomentMotoraPriObrtajima = donjiMoment.Value + (BrojObrtajaMotora - donjiObrtajiZaokrNa100 * 100m) *
+                    ((gornjiMoment.Value - donjiMoment.Value) / 100m);
             }
         }
         public void IzaracunajMomentNaTockovima(decimal glavniPrenos)
         {
-            this.MomentNaTocku = this.MomentMotoraPriObrtajima * this.PrenosniOdnosMenjaca * glavniPrenos;
+            MomentNaTocku = MomentMotoraPriObrtajima * PrenosniOdnosMenjaca * glavniPrenos;
         }
 
     }
--- Version2/ShiftPointCalculator/ShiftPointCalculator/DataAcces/StepenPrenosaMenjacaDataProvider.cs
+++ Version2/ShiftPointCalculator/ShiftPointCalculator/DataAcces/StepenPrenosaMenjacaDataProvider.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using ShiftPointCalculator.BusinessLogic;
 using ShiftPointCalculator.Repositories;
 using System;
 using System.Collections.Generic;
